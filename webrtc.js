067eaf9f05bc29cf3733817f9c63859ac4da49f5
Mike Fuller <emailthemike@gmail.com>
2020-03-31 23:34:07 -0400

    complete rewrite

--- webrtc.js
+++ webrtc.js
@@ -1,250 +1,114 @@
 'use strict';
 
-function WEBRTC(send, stunURLs = []) {
+function WEBRTC(configuration = null) {
 
-  let onState = null;
-  let calls = {};
-
-  let stateHandler = (conn, state) => {
-    if (onState && typeof onState === 'function') {
-      onState(conn, state);
-    }
-  };
-
-  const config = {
-    "iceServers": [{
-      "urls": stunURLs
+  const config = configuration || {
+    iceServers: [{
+      "urls": ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302", "stun:stun3.l.google.com:19302", "stun:stun4.l.google.com:19302"]
     }]
   };
 
-  const sdpConstraints = {};
+  const peer = new RTCPeerConnection(config);
 
-  const ChannelOpen = (e, pc, id) => {
-    stateHandler(calls[id], {
-      "type": "channel",
-      "channel": e.target.label,
-      "message": "Data channel open"
-    });
+  let sendHandler = null;
+
+  const onSend = (cb) => {
+    sendHandler = cb;
   };
 
-  const ChannelAdded = (e, pc, id) => {
-    let label = e.channel.label;
-    calls[id].channels[label] = e.channel;
-    calls[id].channels[label].onopen = (e) => {
-      ChannelOpen(e, pc, id);
-    };
-    return calls[id].channels[label];
-  };
-
-  const remoteTrackAdded = (e, pc, id) => {
-    if (!calls[id].remoteTracks) {
-      calls[id].remoteTracks = [];
+  const send = (e) => {
+    if (sendHandler && typeof sendHandler === 'function') {
+      sendHandler(e);
     }
-    calls[id].remoteTracks.push(e.track);
-    stateHandler(calls[id], {
-      "type": "remoteTrack",
-      "track": e.track,
-      "message": "Remote media track added"
-    });
   };
 
-  const localTrackAdded = (stream, pc, id) => {
-    for (let track of stream.getTracks()) {
-      pc.addTrack(track);
-      calls[id].localTracks.push(track);
-      stateHandler(calls[id], {
-        "type":"localTrack",
-        "track": track,
-        "message":"Local media track added"
+  let eventHandler = null;
+
+  const onEvent = (cb) => {
+    eventHandler = cb;
+  };
+
+  const Event = (m) => {
+    if (eventHandler && typeof eventHandler === 'function') {
+      eventHandler(m);
+    }
+  };
+
+  peer.oniceconnectionstatechange = (e) => {
+    if (e.target.iceConnectionState === 'connected') {
+      Event({
+        "type": "connected"
+      });
+    }
+    if (e.target.iceConnectionState === 'disconnected') {
+      Event({
+        "type": "disconnected"
       });
     }
   };
 
-  const Disconnected = (e, pc, id) => {
-    stateHandler(calls[id], {
-      "type":"disconnected"
-    });
-  };
-
-  const ConnectionChange = (e, pc, id) => {
-    stateHandler(calls[id], {
-      "type":"connectionchange",
-      "state":pc.connectionState
-    });
-  };
-
-  function PEER(id, to) {
-
-    let pc = new RTCPeerConnection(config);
-
-    pc.onclose = (e) => {
-      Disconnected(e, pc, id);
-    };
-
-    pc.ondatachannel = (e) => {
-      ChannelAdded(e, pc, id);
-    };
-
-    pc.ontrack = (e) => {
-      remoteTrackAdded(e, pc, id);
-    };
-
-    let call = {
-      id,
-      "to":to.toString(),
-      pc,
-      "channels":{},
-      "localTracks":[],
-      "remoteTracks":[]
-    };
-
-    calls[id] = call;
-
-    return call;
-  }
-
-  // CALL
-
-  function CALL(to, stream = null) {
-    return new Promise(async (resolve, reject) => {
-
-      let id = crypto.getRandomValues(new Uint8Array(128)).join('').toString().slice(32, 96);
-
-      calls[id] = PEER(id, to);
-      let pc = calls[id].pc;
-
-      calls[id].channels.chat = calls[id].pc.createDataChannel('chat');
-      calls[id].channels.chat.onopen = (e) => {
-        ChannelOpen(e, pc, id);
-      };
-
-      if (stream) {
-        localTrackAdded(stream, pc, id);
-      }
-
-      calls[id].pc.createOffer().then(async desc => {
-        await calls[id].pc.setLocalDescription(desc);
+  peer.onnegotiationneeded = async (e) => {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer),
+      send({
+        "description": peer.localDescription.toJSON()
+      })
+    peer.onicecandidate = async (e) => {
+      if (e.candidate) {
         send({
-          "id": id,
-          "to": to,
-          "type": "call",
-          "stream": (stream) ? true : false,
-          "call": {
-            "desc": calls[id].pc.localDescription
-          }
+          "candidate": e.candidate.toJSON()
         });
-        calls[id].pc.onicecandidate = (e) => {
+      }
+    };
+  };
+
+  peer.ondatachannel = (e => {
+    Event(e);
+  });
+
+  peer.ontrack = (e) => {
+    Event(e);
+  };
+
+  const Listen = async ({
+    description,
+    candidate
+  }) => {
+    if (description) {
+      await peer.setRemoteDescription(description);
+      if (description.type === 'offer') {
+        await peer.setLocalDescription(await peer.createAnswer());
+        send({
+          "description": peer.localDescription.toJSON()
+        })
+        peer.onicecandidate = async (e) => {
           if (e.candidate) {
             send({
-              "id": id,
-              "to": to,
-              "type": "call",
-              "call": {
-                "candidate": e.candidate
-              }
+              "candidate": e.candidate.toJSON()
             });
           }
         };
-        resolve(calls[id]);
-      });
-
-    });
-  }
-
-  // LISTEN
-
-  async function LISTEN(call) {
-    if (!call && !call.id && !call.type === 'call') {
-      return null;
-    }
-
-    if (!calls[call.id]) {
-      calls[call.id] = PEER(call.id, call.from);
-    }
-
-    if (call.call.desc) {
-      const offerDesc = new RTCSessionDescription(call.call.desc);
-      await calls[call.id].pc.setRemoteDescription(offerDesc);
-      stateHandler(calls[call.id], {"type":"incoming", "call":call});
-    }
-
-    if (call.call.candidate) {
-      await calls[call.id].pc.addIceCandidate(new RTCIceCandidate(call.call.candidate)).catch(err=>{console.log(err);return null;});
-    }
-
-  }
-
-  // ANSWER
-
-  function ANSWER(call, stream) {
-    return new Promise(async (resolve, reject) => {
-
-      let id = call.id || null;
-
-      if (!calls[id]) {
-        return null;
       }
-
-      if (stream) {
-        localTrackAdded(stream, calls[id].pc, id);
-      }
-
-      calls[id].pc.createAnswer().then(async answerDesc=>{
-        await calls[id].pc.setLocalDescription(answerDesc);
-        send({
-          "id": id,
-          "to": calls[id].to,  
-          "type": "answer",
-          "answer": {
-            "desc": answerDesc
-          }
-        });
-        calls[id].pc.onicecandidate = (e) => {
-          if (e.candidate) {
-            send({
-              "id": id,
-              "to": calls[id].to,
-              "type": "answer",
-              "call": {
-                "candidate": e.candidate
-              }
-            });
-          }
-        };
-        resolve(calls[id]);
-      });
-
-    });
-  }
-
-  // GOT ANSWER
-
-  async function gotAnswer(answer) {
-    if (!answer && !answer.id && !calls[answer.id] && answer.type !== 'answer') {
-      return null;
+    } else if (candidate) {
+      await peer.addIceCandidate(candidate);
     }
-    const conn = calls[answer.id];
+  };
 
-    if (answer.answer && answer.answer.desc) {
-      const answerDesc = new RTCSessionDescription(answer.answer.desc);
-      await conn.pc.setRemoteDescription(answerDesc);
-    }
+  const AddTrack = async (track, stream) => {
+    await peer.addTrack(track, stream);
+  };
 
-    if (answer.answer && answer.answer.candidate) {
-      await conn.pc.addIceCandidate(new RTCIceCandidate(answer.answer.candidate)).catch(err=>{console.log(err);return null;});
-    }
-
-  }
+  const CreateDataChannel = async (label = null, options = {}) => {
+    return peer.createDataChannel(label, options);
+  };
 
   return {
-    "calls": calls,
-    "call": CALL,
-    "listen": LISTEN,
-    "answer": ANSWER,
-    "gotAnswer": gotAnswer,
-    "onState": (cb) => {
-      onState = cb;
-    }
+    "pc": peer,
+    "createDataChannel": CreateDataChannel,
+    "addTrack": AddTrack,
+    "listen": Listen,
+    "onSend": onSend,
+    "onEvent": onEvent
   };
 
 }
